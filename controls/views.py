9593f993db62ad0638bac150860624b8539177ae
Samuel123-456 <https://github.com/Samuel123-456/pharma-master-.git>
2025-06-07 21:36:21 +0100

    signin and signup functionalities done v-0.1

--- controls/views.py
+++ controls/views.py
@@ -3,11 +3,28 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.db.models import Q
+from django.contrib.auth import login, authenticate
 
 
 # Create your views here.
 def signin(request):
       template_name = 'controls/signin.html'
+      
+      if request.method == 'POST':
+            email = request.POST.get('email')
+            password = request.POST.get('password')
+
+            user = User.objects.filter(email=email).first()            
+            user_authenticated = authenticate(request, username=user.username, password=password)      
+
+            if not user_authenticated:
+                  messages.add_message(request, messages.ERROR, 'Credencias invalida')
+                  return redirect(reverse('signin'))
+            
+            login(request, user)
+            messages.add_message(request, messages.SUCCESS, 'Login feito com sucesso')
+            
+            return render(request, 'site/dashboard.html')
       return render(request, template_name)
 
 #TODO: FAZER VALIDACOE AVANCADAS
