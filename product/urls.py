9bdcc25a3227747fe1c2027a74f5bea96aa5d56b
Samuel123-456 <https://github.com/Samuel123-456/pharma-master-.git>
2025-06-17 23:38:28 +0100

    remove all urls and add create-product url for new updates

--- product/urls.py
+++ product/urls.py
@@ -1,18 +1,12 @@
 from django.urls import path
 from product.views import (
-    create,
-    read,
-    delete,
-    update,
-    selling,
-    dashboard
+    ProductCreateView
 )
+from django.contrib.auth.decorators import login_required
+
+
 
 urlpatterns = [
-    path('create/', create, name='create_product'),
-    path('read/', read, name='read_product'),
-    path('delete/<slug:slug>', delete, name='delete_product'),
-    path('update/<slug:slug>', update, name='update_product'),
-    path('selling/<slug:slug>', selling, name='sell_product'),
-    path('dashboard/', dashboard, name='dashboard'),
+    path('create/', login_required(ProductCreateView.as_view(), login_url='signin'), name='create-product')
 ]
+    
